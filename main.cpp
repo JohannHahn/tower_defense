81964c7f66921ef246aae32cc0214e38d9926cf1
Johann Hahn <wanische@gmail.com>
2025-05-11 23:09:16 +0200

    raylib include

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+//#include "raylib/raylib/include/raylib.h"
 #include "raylib.h"
 
 int main() {
+    InitWindow(300, 600, "Tower Defense"); 
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(BLUE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }

f3702881d798d36be0f5d2b68574fbb4323bbd3d
Johann Hahn <johann-hahn@yandex.ru>
2025-05-13 22:20:57 +0200

    first building blocks

--- main.cpp
+++ main.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
 //#include "raylib/raylib/include/raylib.h"
 #include "raylib.h"
+#include <cassert>
+#include <vector>
+
+struct Window {
+    int width = 900;
+    int height = 900;
+    const char* title = "Tower Defense";
+
+    void init() {
+        InitWindow(width, height, title);
+    }
+
+    void close() {
+        CloseWindow();
+    }
+};
+
+struct Map {
+    // ground texture
+    // road / waypoints
+    // buildings_mask
+};
+
+enum Enemy_Type {
+    CHICKEN,
+};
+
+struct Enemy {
+    float hp;
+    float speed;
+    float damage;
+    float max_speed;
+    Enemy_Type type;
+};
+
+enum Building_Type {
+    HOLE,
+};
+
+struct Building {
+    float hp;
+    float damage;
+    float range;
+    Building_Type type;
+};
+
+struct Level {
+    Map map;
+    std::vector<Enemy> enemies;
+    std::vector<Building> buildings;
+};
 
 int main() {
-    InitWindow(300, 600, "Tower Defense"); 
+    Window window;
+
+    window.init();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -13,6 +66,7 @@ int main() {
         EndDrawing();
     }
 
-    CloseWindow();
+    window.close();
+
     return 0;
 }
